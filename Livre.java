085bcad313d3edd921cf41848bd32f7079cee356
YannickGosset <yannick.gosset@etu.univ-amu.fr>
2017-04-05 10:42:43 +0200

    Classe Livre crée
    Création de la classe livre

---
+++ Livre.java
@@ -0,0 +1,24 @@
+package com.bibliotheque.livres;
+
+public class Livre {
+	
+	private String titre;
+	private int idLivre;
+	private boolean estDisponible;
+	
+	public Livre(String titre, int idLivre, boolean estDisponible) {
+		super();
+		this.titre = titre;
+		this.idLivre = idLivre;
+		this.estDisponible = estDisponible;
+	}
+
+	public void setEstDisponible(boolean estDisponible) {
+		this.estDisponible = estDisponible;
+	}
+
+	public boolean isEstDisponible() {
+		return estDisponible;
+	}
+	
+}
