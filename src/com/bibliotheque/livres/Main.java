f082427505981408ffcca8a7d828335b7e1ea990
YannickGosset <yannick.gosset@etu.univ-amu.fr>
2017-04-07 16:42:31 +0200

    Fin du main
    Finalisation du mais test d'emprunt d'un livre puis notification après 10 secondes.

--- src/com/bibliotheque/livres/Main.java
+++ src/com/bibliotheque/livres/Main.java
@@ -10,13 +10,16 @@ public class Main {
 		Livre livre3 = new Livre("Harry Potter2", 2, true, "fantaisie");
 		
 		ArrayList<Livre> livresDisponibles = new ArrayList<Livre>();
+		ArrayList<Livre> livresIndisponibles = new ArrayList<Livre>();
 		
-		Stock stock = new Stock(livresDisponibles, null);
+		Stock stock = new Stock(livresDisponibles, livresIndisponibles);
 		stock.ajouterLivreDisponible(livre1);
 		stock.ajouterLivreDisponible(livre2);
 		stock.ajouterLivreDisponible(livre3);
 		
 		Client client = new Client();
+		ArrayList<Emprunt> emprunts = new ArrayList<Emprunt>();
+		client.setEmprunts(emprunts);
 		client.emprunter(stock.getLivresDisponibles().get(0));
 		stock.supprimerLivreDesDiponibles(livre1);
 		stock.ajouterLivreIndisponible(livre1);
@@ -25,8 +28,5 @@ public class Main {
 		for(Emprunt e : client.getEmprunts()){
 			administrateur.voirDateRetour(e);
 		}
-		
-		client.notifier();
-		
 	}
 }
